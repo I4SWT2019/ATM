7833f3a15ea2bca6311f2cd96e68cbfac3a85f52
Morten Filt <201600311@post.au.dk>
2019-10-10 09:56:18 +0200

    Added test solution

---
+++ ATM/TestClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM
+{
+    public class TestClass
+    {
+        public TestClass() { }
+
+        public bool Test()
+        {
+            return true;
+        }
+    }
+}
