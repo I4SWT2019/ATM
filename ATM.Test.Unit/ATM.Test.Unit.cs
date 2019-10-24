b1574969751956e9a05e70d9ea867d1c0b82602d
Morten Filt <201600311@post.au.dk>
2019-10-24 12:09:09 +0200

    Test#2

---
+++ ATM.Test.Unit/ATM.Test.Unit.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace ATM.Test.Unit
+{
+    [TestFixture]
+    [Author("I4SWT2019Gr2")]
+    public class ATMUnitTest
+    {
+        private TestClass _uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _uut = new TestClass();
+        }
+
+        [Test]
+        public void Test_ReturnsTrue()
+        {
+            Assert.That(_uut.Test(),Is.True);
+        }
+    }
+}
