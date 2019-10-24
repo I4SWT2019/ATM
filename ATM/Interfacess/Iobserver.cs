5a3bce16211496c88908b7738108fb88fed88171
Mikkel Rahbek <Mikkel.rahbek@gmail.com>
2019-10-24 12:45:17 +0200

    Added multiple interfaces to interfacess

--- ATM/Interfacess/Iobserver.cs
+++ ATM/Interfacess/Iobserver.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ATM.Interfaces
+namespace ATM.Interfacess
 {
-    class ICalculator
+    class Iobserver
     {
     }
 }
