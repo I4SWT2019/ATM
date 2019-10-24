ddd150582a5bd72fd51005ea658b9555026ee3bc
Morten Filt <201600311@post.au.dk>
2019-10-24 13:03:50 +0200

    Changed ATM

--- ATM/ATM.cs
+++ ATM/ATM.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ATM.Interfacess;
 
 namespace ATM
 {
-    class ATM
+    public class ATM : IObserver
     {
+        private List<Plane> _planes;
+        private List<Plane> _previousPlanes;
+        private ICalculator _velocityCalc;
+        private ICalculator _headingCalc;
     }
 }
