8ccf2fb85ace242f8f723229b1ec6e75e1eeab46
Mikkel Rahbek <Mikkel.rahbek@gmail.com>
2019-10-24 21:28:14 +0200

    Added Plane

--- ATM/DataHandler.cs
+++ ATM/DataHandler.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TransponderReceiver;
 
 namespace ATM
 {
     class DataHandler
     {
+        private void ReceiverOnTransponderDataReady(object sender, RawTransponderDataEventArgs e)
+        {
+            // Just display data
+            foreach (var data in e.TransponderData)
+            {
+                System.Console.WriteLine($"Transponderdata {data}");
+            }
+        }
     }
 }
--- ATM/Plane.cs
+++ ATM/Plane.cs
@@ -6,8 +6,16 @@ using System.Threading.Tasks;
 
 namespace ATM
 {
-    class Plane
+    public class Plane
     {
-     
+        private string _tag;
+        private int _altitude, _longdetude, _latitude, _timestamp, _heading;
+        private double _velocity;
+
+        public void SetAll(string tag, int altitude, int longdetude, int latitude, int timestamp, int heading, double velocity)
+        {
+
+        }
     }
+
 }
