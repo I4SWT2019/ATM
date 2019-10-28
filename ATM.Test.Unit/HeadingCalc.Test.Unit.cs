59b0233649b39f568737edd113ecf518d2d93152
Winckler <au534747@post.au.dk>
2019-10-28 17:05:05 +0100

    Added heading to the mix, w. tests.

---
+++ ATM.Test.Unit/HeadingCalc.Test.Unit.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using ATM.Calculators;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace ATM.Test.Unit
+{
+    [TestFixture]
+    public class HeadingCalcTestUnit
+    {
+
+        HeadingCalculator uut;
+
+
+        [SetUp]
+        public void Setup()
+        { uut = new HeadingCalculator();}
+
+        [Test]
+        public void Calculate_ReceivedZeroAsDataPoints_ReturnZero()
+        {
+
+            //Arrange
+            int[] oldData = new int[] 
+                {0000, 0000};
+
+            int[] newData = new int[]
+                {0000, 0000};
+
+            double Heading = 0;
+            //Act
+
+            Heading = uut.Calculate(oldData, newData);
+
+            //Assert
+
+            Assert.That(Heading, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Calculate_ReceivedTwoDataPoints_ReturnAngle()
+        {
+
+            //Arrange
+            int[] oldData = new int[]
+                {0000, 0000};
+                // x     y
+            int[] newData = new int[]
+                {0000, 0001};
+
+            double Heading = 0;
+            //Act
+
+            Heading = uut.Calculate(oldData, newData);
+
+            //Assert
+
+            Assert.That(Heading, Is.EqualTo(90));
+        }
+
+        [Test]
+        public void Calculate_ReceivedDataPoints_ReturnNegativeAngle()
+        {
+
+            //Arrange
+            int[] oldData = new int[]
+                {0000, 0001};
+                // x     y
+            int[] newData = new int[]
+                {0000, 0000};
+
+            double Heading = 0;
+            //Act
+
+            Heading = uut.Calculate(oldData, newData);
+
+            //Assert
+
+            Assert.That(Heading, Is.EqualTo(270.00));
+        }
+
+        [Test]
+        public void Calculate_ReceivedDataPoints_ReturnBiggerAngle()
+        {
+
+            //Arrange
+            int[] oldData = new int[]
+                {0000, 0001};
+                // x     y
+            int[] newData = new int[]
+                {0001, 0000};
+
+            double Heading = 0;
+            //Act
+
+            Heading = uut.Calculate(oldData, newData);
+
+            //Assert
+
+            Assert.That(Heading, Is.EqualTo(315.00));
+        }
+    }
+}
--- ATM/Calculators/HeadingCalculator.cs
+++ ATM/Calculators/HeadingCalculator.cs
@@ -9,10 +9,27 @@ namespace ATM.Calculators
 {
     public class HeadingCalculator : ICalculator
     {
-        public double Calculate(int[] x, int[] y)
+        public double Calculate(int[] oldData, int[] newData)
         {
-            double Heading = 0;
+            double diffX = 0, diffY = 0, Radians = 0, Angle = 0, Heading = 0;
 
+
+            //reverse here, as the order of reading is reversed from the math notation
+            diffX = newData[0] - oldData[0];
+            diffY = newData[1] - oldData[1];
+
+
+            Radians = Math.Atan2(diffY, diffX);
+
+            Angle = Radians * (180 / Math.PI);
+
+            if (Angle < 0)
+            {
+                Angle += 360;
+            }
+        
+
+            Heading = Angle;
 
             return Heading;
         }
