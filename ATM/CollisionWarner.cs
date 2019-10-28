afab99f9c051b86b0fbcc365dac1f5e4ecb5e52f
Morten Filt <201600311@post.au.dk>
2019-10-28 13:19:04 +0100

    Added part of CollisionWarner

--- ATM/CollisionWarner.cs
+++ ATM/CollisionWarner.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ATM.Interfacess;
 
 namespace ATM
 {
-    class CollisionWarner
+    public class CollisionWarner : ISubscriber
     {
+        private IPrinter _seperationPrinter;
+        public void Update(List<Plane> planes)
+        {
+            WarningCheck(planes);
+        }
+
+        private void Print(List<Plane> planes)
+        {
+            _seperationPrinter.Print(planes);
+        }
+
+        private void WarningCheck(List<Plane> planes)
+        {
+            bool collisionImminent = false;
+
+            if (collisionImminent == true)
+            { }
+        }
     }
 }
