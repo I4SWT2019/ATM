78d599a60305f305aa88efba308ba39ebdd2f48c
Mikkel Rahbek <Mikkel.rahbek@gmail.com>
2019-10-31 13:11:09 +0100

    More test edits

--- ATM.Test.Unit/DataHandler.Test.Unit.cs
+++ ATM.Test.Unit/DataHandler.Test.Unit.cs
@@ -17,14 +17,13 @@ namespace ATM.Test.Unit
     {
         private DataHandler _uut;
         private PlaneAddedEventArgs _receivedEventArgs;
-        private ITransponderReceiver _transponderReceiver;
 
         [SetUp]
         public void Setup()
         {
             _receivedEventArgs = null;
 
-            _uut = new DataHandler(_transponderReceiver);
+            _uut = new DataHandler();
             _uut.HandleData("XYZ987;25059;75654;4000;20151006213456789");
 
             _uut.PlaneAddedEvent +=
