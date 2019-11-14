9b0ba0805d788c2580ccde822c89c57610f12ce5
Morten Filt <201600311@post.au.dk>
2019-11-14 10:11:36 +0100

    Changed ATM test

--- ATM.Test.Unit/ATM.Test.Unit.cs
+++ ATM.Test.Unit/ATM.Test.Unit.cs
@@ -32,6 +32,17 @@ namespace ATM.Test.Unit
             _plane2 = new Plane();
             _plane3 = new Plane();
 
+            _plane1._tag = "ABC123";
+            _plane2._tag = "DEF123";
+            _plane3._tag = _plane1._tag;
+            _plane1._timestamp = "xxxxxxxxxxxx00003";
+            _plane2._timestamp = "xxxxxxxxxxxx00001";
+            _plane3._timestamp = "xxxxxxxxxxxx00001";
+            _plane1._latitude = 1;
+            _plane1._longitude = 1;
+            _plane3._latitude = 3;
+            _plane3._longitude = 3;
+
             _uut = new ATM(_areaPrinter, _velocityCalculator, _headingCalculator);
         }
 
@@ -131,16 +142,6 @@ namespace ATM.Test.Unit
         public void CalcVelocity_VelocityCalculatorCalledWithValues()
         {
             // Arrange
-            _plane1._tag = "ABC123";
-            _plane2._tag = "DEF123";
-            _plane3._tag = _plane1._tag;
-            _plane1._timestamp = "xxxxxxxxxxxx00003";
-            _plane2._timestamp = "xxxxxxxxxxxx00001";
-            _plane3._timestamp = "xxxxxxxxxxxx00001";
-            _plane1._latitude = 1;
-            _plane1._longitude = 1;
-            _plane3._latitude = 3;
-            _plane3._longitude = 3;
             int[] p1 = new int[] {1, 1, 00003};
             int[] p3 = new int[] {3, 3, 00001};
             List<Plane> previousPlanes = new List<Plane>()
@@ -220,13 +221,7 @@ namespace ATM.Test.Unit
         public void CalcHeading_HeadingCalculatorCalledWithValues()
         {
             // Arrange
-            _plane1._tag = "ABC123";
-            _plane2._tag = "DEF123";
-            _plane3._tag = _plane1._tag;
-            _plane1._latitude = 1;
-            _plane1._longitude = 1;
-            _plane3._latitude = 3;
-            _plane3._longitude = 3;
+           
             int[] p1 = new int[] { 1, 1 };
             int[] p3 = new int[] { 3, 3 };
             List<Plane> previousPlanes = new List<Plane>()
